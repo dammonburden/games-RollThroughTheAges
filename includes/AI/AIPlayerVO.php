a5045099288a9d69b32840f4f57b9df9d3eb72f5
Dammon R. Burden <dammonburden@gmail.com>
2017-05-03 17:57:16 -0700

    Capitalization, Testing push / pull

--- includes/AI/AIPlayerVO.php
+++ includes/AI/AIPlayerVO.php
@@ -6,11 +6,11 @@ use RTTA\Player\PlayerVO;
 class AIPlayerVO extends PlayerVO
 {
     // TODO:
-    // dear future self,
-    // this isn't working because your autoloader isn't working properly. It has no AI, and this is the first
+    // Dear future self,
+    // This isn't working because your autoloader isn't working properly. It has no AI, and this is the first
     // time you are requiring a file (not just using a namespace), which is referring to something LATER in the auto
     // load queue. I'm sure you can find ways to deal with this, just stay calm, sleep, focus, etc.
-    // And of course, consult google.
+    // And of course, consult Google. ~ Logan (Dammon agrees)
     const MIND_EASY = 'easy';
 
     public $mind;
